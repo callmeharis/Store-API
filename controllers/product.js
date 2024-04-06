58138d6b199df08c7185801125ae529ca55aac3c
callmeharis <theharis99@gmail.com>
2024-04-06 21:34:16 +0500

    STORE-API-04 || filtering and sorting products through regex

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,15 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({ featured: true })
+    const search = "ha"
+    // const products = await Product.find({
+    //     name: { $regex: search, $options: "i" }
+    // })
+    const products = await Product.find({}).sort("-name price")
     res.status(200).json({ products, nbHits: products.length })
 }
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort } = req.query;
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === "true" ? true : false
@@ -14,10 +18,19 @@ const getAllProducts = async (req, res) => {
         queryObject.company = company
     }
     if (name) {
-        queryObject.name = name
+        queryObject.name = { $regex: name, $options: "i" }
     }
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
+
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(",").join(" ")
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort("createdAt")
+    }
+
+    const products = await result;
     res.status(200).json({ products, nbHits: products.length })
 }
 module.exports = { getAllProductsStatic, getAllProducts }
